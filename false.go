28566d77ad0e50f24aa4b061c58acbfc87126f9b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-14 13:41:52 -0700

    false true

--- false.go
+++ false.go
@@ -2,6 +2,8 @@ package qry
 
 type False[T any] struct {}
 
+var _ Evaluator[string] = False[string]{}
+
 func (receiver False[T]) Evaluate(value T) (bool, error) {
 	return false, nil
 }
--- true.go
+++ true.go
@@ -2,6 +2,8 @@ package qry
 
 type True[T any] struct {}
 
+var _ Evaluator[string] = True[string]{}
+
 func (receiver True[T]) Evaluate(value T) (bool, error) {
 	return true, nil
 }
